ed6305d242413cf036a91535addb8b9d94d27f08
Rodrigo Ferreira de Souza <rodrigo.ferreira@deal.com.br>
2022-09-18 12:30:10 -0300

    Implemented Rest Template

---
+++ hr-payroll/src/main/java/br/com/cronos/hrpayroll/entities/Worker.java
@@ -0,0 +1,63 @@
+package br.com.cronos.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+
+    private String name;
+
+    private double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(UUID id, String name, double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return id.equals(worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- hr-payroll/src/main/java/br/com/cronos/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/br/com/cronos/hrpayroll/service/PaymentService.java
@@ -1,14 +1,30 @@
 package br.com.cronos.hrpayroll.service;
 
 import br.com.cronos.hrpayroll.entities.Payment;
+import br.com.cronos.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+    private final RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(UUID workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", "" + workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
